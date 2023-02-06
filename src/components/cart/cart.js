2bcfd8b25d7337aae947cbf0dd628efadeded50c
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-06 19:28:30 +0600

    adding better feedback to user when sending data to firebase database

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import classes from "./cart.module.css";
 import Modal from "../ui/modal/modal";
 import CartContext from "../../store/cart-context";
@@ -7,6 +7,9 @@ import CheckOut from "./check-out/check-out";
 
 const Cart = (props) => {
     const [isCheckout, setIsCheckout] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
+
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItem = cartCtx.items.length > 0;
@@ -23,14 +26,18 @@ const Cart = (props) => {
         setIsCheckout(true);
     };
 
-    const submitOrderHandler = (userData) => {
-        fetch('https://order-food-eb0b9-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
+    const submitOrderHandler = async (userData) => {
+        setIsSubmitting(true);
+        await fetch('https://order-food-eb0b9-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json', {
             method: 'POST',
             body: JSON.stringify({
                 user: userData,
                 orderedItems: cartCtx.items
             })
         });
+        setIsSubmitting(false);
+        setDidSubmit(true);
+        cartCtx.clearCart();
     };
 
     const cartItem = (
@@ -57,8 +64,8 @@ const Cart = (props) => {
         </div>
     );
 
-    return (
-        <Modal onClose={props.onClose}>
+    const cartModalContent = (
+        <Fragment>
             {cartItem}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -66,6 +73,26 @@ const Cart = (props) => {
             </div>
             {isCheckout && <CheckOut onConfirm={submitOrderHandler} onCancel={props.onClose} />}
             {!isCheckout && modalActions}
+        </Fragment>
+    );
+
+    const isSubmittingModalContent = <p>Order data is submitting...</p>;
+    const didSubmitModalContent = (
+        <Fragment>
+            <p>Successfully sent the order!</p>
+            <div className={classes.actions}>
+                <button className={classes.button} onClick={props.onClose}>
+                    Close
+                </button>
+            </div>
+        </Fragment>
+    );
+
+    return (
+        <Modal onClose={props.onClose}>
+            {!isSubmitting && !didSubmit && cartModalContent}
+            {isSubmitting && isSubmittingModalContent}
+            {!isSubmitting && didSubmit && didSubmitModalContent}
         </Modal>
     );
 }
